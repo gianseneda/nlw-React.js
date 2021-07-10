7c6b93bab658f94ecb876feaf191f3acfc98c77c
gian seneda <gian_seneda@hotmail.com>
2021-07-10 07:51:15 -0300

    início da explicacao de typescript

--- Ignite/ReactJs/01-github-explorer/src/components/RepositoryList.jsx
+++ Ignite/ReactJs/01-github-explorer/src/components/RepositoryList.jsx
@@ -1,21 +1,32 @@
 import { RepositoryItem } from "./RepositoryItem";
+import { useState, useEffect } from "react";
+import "../styles/repositories.scss";
 
-const repository = {
-  name: "unform",
-  description: "Forms in React",
-  link: "http://github.com/unform/unform",
-};
+//useEffect dispara função quando algo acontece na aplicação, por exemplo
+
+// https://api.github.com/orgs/rocketseat/repos
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]); //estado inicial é uma lista, no caso, vazia, então []
+
+  useEffect(() => {
+    fetch("https://api.github.com/orgs/rocketseat/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []); //função e quando executar (neste caso, quando houver mudança no array repositories) - NUNCA deixar sem o último parâmetro pois ele vai executar em loop
+
   return (
     <section className="repository-list">
       <h1>Lista de Repositórios</h1>
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
+        {repositories.map((repository) => {
+          return (
+            <RepositoryItem key={repository.name} repository={repository} />
+          );
+        })}
       </ul>
     </section>
   );
 }
+
+//key é necessário no MAP para que ele se localize melhor com uma chave única. Nesse caso, o nome é sempre único
