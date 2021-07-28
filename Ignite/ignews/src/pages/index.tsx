c26a43e9a5fd7176c76ee98374a37821a5f41a6a
gianseneda <gian_seneda@hotmail.com>
2021-07-27 22:02:40 -0300

    Inicio do ambiente com estilo globais do IGnews

---
+++ Ignite/ignews/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início | IGNews</title>
+      </Head>
+      <h1>Hello World</h1>
+    </>
+  );
+}
