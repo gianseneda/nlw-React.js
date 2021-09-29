73f834611143024aa6c465189eaeb699ad239d4b
gianseneda <gian_seneda@hotmail.com>
2021-09-28 22:50:32 -0300

    navegação para a pagina de POST que agora consume a API do prismic

--- Ignite/ignews/src/components/Header/index.tsx
+++ Ignite/ignews/src/components/Header/index.tsx
@@ -1,15 +1,23 @@
 import { SignInButton } from "../SignInButton/index";
+import { useRouter } from "next/router";
+import { ActiveLink } from "../ActiveLink";
 
 import styles from "./styles.module.scss";
 
 export function Header() {
+  const { asPath } = useRouter();
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="./logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a href="">Posts</a>
+          <ActiveLink href="/" activeClassName={styles.active}>
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink href="/posts" activeClassName={styles.active}>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
         <SignInButton />
       </div>
--- Ignite/ignews/src/pages/posts/index.tsx
+++ Ignite/ignews/src/pages/posts/index.tsx
@@ -3,8 +3,20 @@ import Head from "next/head";
 import { GetStaticProps } from "next";
 import Prismic from "@prismicio/client";
 import { getPrismicClient } from "../../services/prismic";
+import { RichText } from "prismic-dom";
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+interface PostosProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostosProps) {
   return (
     <>
       <Head>
@@ -12,39 +24,13 @@ export default function Posts() {
       </Head>
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Utilizando Path Mapping no TypeScript</strong>
-            <p>
-              Lidar com importação de arquivos em uma aplicação de médio a
-              grande porte com Node.js é trabalhoso. A IDE às vezes não é auto
-              suficiente e começa a bugar nessa hora. Temos também o problema do
-              "SlashPathHell": ../../../path/to/file.js, onde colocamos várias
-              "../" para descer ou subir de nível nos diretórios.
-            </p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Utilizando Path Mapping no TypeScript</strong>
-            <p>
-              Lidar com importação de arquivos em uma aplicação de médio a
-              grande porte com Node.js é trabalhoso. A IDE às vezes não é auto
-              suficiente e começa a bugar nessa hora. Temos também o problema do
-              "SlashPathHell": ../../../path/to/file.js, onde colocamos várias
-              "../" para descer ou subir de nível nos diretórios.
-            </p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Utilizando Path Mapping no TypeScript</strong>
-            <p>
-              Lidar com importação de arquivos em uma aplicação de médio a
-              grande porte com Node.js é trabalhoso. A IDE às vezes não é auto
-              suficiente e começa a bugar nessa hora. Temos também o problema do
-              "SlashPathHell": ../../../path/to/file.js, onde colocamos várias
-              "../" para descer ou subir de nível nos diretórios.
-            </p>
-          </a>
+          {posts.map((post) => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -55,16 +41,32 @@ export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient();
 
   const response = await prismic.query(
-    [Prismic.predicates.at("document.type", "Post")],
+    [Prismic.predicates.at("document.type", "post")],
     {
-      fetch: ["Post.title", "Post.content"],
+      fetch: ["post.title", "post.content"],
       pageSize: 100,
     }
   );
 
-  console.log(JSON.stringify(response, null, 2));
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === "paragraph")
+          ?.text ?? "", //primeiro parágrafo ou string vazia
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        "pt-BR",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+      ),
+    };
+  });
 
   return {
-    props: {},
+    props: { posts },
   };
 };
