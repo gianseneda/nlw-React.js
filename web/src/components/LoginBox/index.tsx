b4730c5fd321a4613543a326136b56c993215a6f
gianseneda <gian_seneda@hotmail.com>
2021-11-13 20:41:08 -0300

    inicio da autenticação com um erro no MAP do componente messageList

--- web/src/components/LoginBox/index.tsx
+++ web/src/components/LoginBox/index.tsx
@@ -1,11 +1,48 @@
 import styles from "./styles.module.scss";
 import { VscGithubInverted } from "react-icons/vsc";
+import { useEffect } from "react";
+import { api } from "../../services/api";
+
+type AuthResponse = {
+  token: string;
+  user: {
+    id: string;
+    avatar_url: string;
+    name: string;
+    login: string;
+  };
+};
 
 export function LoginBox() {
+  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=69a9f2d171792f55a31b`;
+
+  async function signIn(githubCode: string) {
+    const response = await api.post<AuthResponse>("authenticate", {
+      code: githubCode,
+    });
+
+    const { token, user } = response.data;
+
+    localStorage.setItem("@dowhile:toke", token);
+  }
+
+  useEffect(() => {
+    const url = window.location.href;
+    const hasGithubCode = url.includes("?code=");
+
+    if (hasGithubCode) {
+      const [urlWithoutCode, githubCode] = url.split("?code=");
+
+      window.history.pushState({}, "", urlWithoutCode);
+
+      githubCode;
+    }
+  }, []);
+
   return (
     <div className={styles.loginBoxWrapper}>
       <strong>Entre e compartilhe sua mensagem</strong>
-      <a href="" className={styles.signInWithGitHub}>
+      <a href={signInUrl} className={styles.signInWithGitHub}>
         <VscGithubInverted size="24" />
         Entrar com GitHub
       </a>
