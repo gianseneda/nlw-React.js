daccd7aea87c34111d669217a32b5305a884242a
gianseneda <gian_seneda@hotmail.com>
2021-12-06 09:56:53 -0300

    término da app front-end web

--- web/src/components/LoginBox/index.tsx
+++ web/src/components/LoginBox/index.tsx
@@ -1,43 +1,10 @@
 import styles from "./styles.module.scss";
 import { VscGithubInverted } from "react-icons/vsc";
-import { useEffect } from "react";
-import { api } from "../../services/api";
-
-type AuthResponse = {
-  token: string;
-  user: {
-    id: string;
-    avatar_url: string;
-    name: string;
-    login: string;
-  };
-};
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../../context/auth";
 
 export function LoginBox() {
-  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=69a9f2d171792f55a31b`;
-
-  async function signIn(githubCode: string) {
-    const response = await api.post<AuthResponse>("authenticate", {
-      code: githubCode,
-    });
-
-    const { token, user } = response.data;
-
-    localStorage.setItem("@dowhile:toke", token);
-  }
-
-  useEffect(() => {
-    const url = window.location.href;
-    const hasGithubCode = url.includes("?code=");
-
-    if (hasGithubCode) {
-      const [urlWithoutCode, githubCode] = url.split("?code=");
-
-      window.history.pushState({}, "", urlWithoutCode);
-
-      githubCode;
-    }
-  }, []);
+  const { signInUrl } = useContext(AuthContext);
 
   return (
     <div className={styles.loginBoxWrapper}>
--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -1,4 +1,5 @@
 import styles from "./styles.module.scss";
+import io from "socket.io-client";
 import { api } from "../../services/api";
 
 import logoImg from "../../assets/logo.svg";
@@ -13,8 +14,29 @@ type Message = {
   };
 };
 
+const messagesQueue: Message[] = [];
+
+const socket = io("http://localhost:4000");
+
+socket.on("new_message", (newMessage: Message) => {
+  messagesQueue.push(newMessage);
+});
+
 export function MessageList() {
   const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (messagesQueue.length > 0) {
+        setMessages((prevState) =>
+          [messagesQueue[0], prevState[0], prevState[1]].filter(Boolean)
+        ); //filter remove os valores falses, undefined ou null
+
+        messagesQueue.shift(); //remove os items da lista
+      }
+    }, 3000);
+  }, []);
+
   useEffect(() => {
     //chamada para API
     api.get<Message[]>("messages/last3").then((response) => {
@@ -27,19 +49,23 @@ export function MessageList() {
       <img src={logoImg} alt="DoWhile 2021" />
 
       <ul className={styles.messageList}>
-        {messages.map((message) => {
-          return (
-            <li className={styles.message} key={message.id}>
-              <p className={styles.messageContent}>{message.text}</p>
-              <div className={styles.messageUser}>
-                <div className={styles.userImage}>
-                  <img src={message.user.avatar_url} alt={message.user.name} />
+        {messages.length &&
+          messages.map((message) => {
+            return (
+              <li className={styles.message} key={message.id}>
+                <p className={styles.messageContent}>{message.text}</p>
+                <div className={styles.messageUser}>
+                  <div className={styles.userImage}>
+                    <img
+                      src={message.user.avatar_url}
+                      alt={message.user.name}
+                    />
+                  </div>
+                  <span>{message.user.name}</span>
                 </div>
-                <span>{message.user.name}</span>
-              </div>
-            </li>
-          );
-        })}
+              </li>
+            );
+          })}
       </ul>
     </div>
   );
